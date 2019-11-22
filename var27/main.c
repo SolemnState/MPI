dcd0b1162980248e8e153d1352b5b7460e1c45ef
Konstantin <lolkazavr1@gmail.com>
2019-11-22 20:59:02 +0300

    new new

--- var27/main.c
+++ var27/main.c
@@ -15,7 +15,6 @@
 #include <string.h>
 
 #define CYCLES 5
-#define NUMBER 5
 
 int main(int argc, char** argv)
 {   
@@ -26,49 +25,53 @@ int main(int argc, char** argv)
     
     // Функция инициализации MPI
     MPI_Init(&argc, &argv);
+
     // Указывает на число процессов в коммуникаторе
     // для MPI_COMM_WORLD указывает на общее количество процессов
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     // Указывает на номер процесса в частной группе коммуникатора
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     // Если запущено не 3 процесса, то выход из программы
-    if (size != 3){
+    if (size != 2){
         if (rank == 0)
-        printf("Error: 3 processes required,\
+        printf("Error: 2 processes required,\
         instead of %d\n", size);
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER);
     return -1;
     }
-    if (rank == 1)
-         number = NUMBER;
-    if (rank == 0)
-        printf("Our number: %d\n", number);
+        
     // Цикл    
     for (int n = 0; n < cycles; n++)
     {
         switch(rank)
         {
-        case 1: // первый процесс
+        case 0: // первый процесс
         {
+            if (n == 0)
+            {
+                printf("Enter number: \n");
+                fflush(stdout);
+                scanf("%d", &number);
+            }
             printf("Iteration number: %d\n", n+1);
             // генерация рандомного числа и отправка второму процессу
-            MPI_Send(&number, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+            MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
             // получение числа от второго процесса
-            MPI_Recv(&number, 1, MPI_INT, 2, 0, MPI_COMM_WORLD,
+            MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,
             MPI_STATUS_IGNORE);
             printf("Number from process 2: %d\n", number);
             number += 1;
         }
         break;
-        case 2: // второй процесс
+        case 1: // второй процесс
         { 
             // получение рандомного числа от первого процесса 
-            MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,
+            MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
             MPI_STATUS_IGNORE);
             number *= 2;
             // отправка первому процессу числа, умноженного на 2
-            MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
         }
         break;
         }
